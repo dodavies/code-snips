53cb18487821e1e594cd6df971d6859766054c3f
jimzah <james@colderwood.com>
2017-09-19 23:32:20 +0100

    Post file uploader

---
+++ file.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+from random import choice
+from string import ascii_uppercase, ascii_lowercase, digits
+fileupload = sys.argv[-1]
+enc = sys.argv[1]
+
+if (enc == "e"):
+    print "Generate random name"
+    fname = ''.join(choice(digits + ascii_uppercase + ascii_lowercase) for i in range(64))
+    fname = fname+fileupload[fileupload.find('.'):]
+else:
+    print "Uploading using file name"
+    fname = fileupload
+
+url = 'https://s.james.ac/fr.php'
+files = {'file': (fname, open(fileupload, 'rb'))}
+
+r = requests.post(url, files=files, auth=('james', '6126'))
+print (r.text)
